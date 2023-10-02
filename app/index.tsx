0f42f41329d3b1fd4e6adbc77e6e31372bc92f68
Jose Quintero <tomark4@gmail.com>
2023-10-02 14:46:44 -0400

    move folders

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 22 }}>Hello!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen options={{}} />
+    </SafeAreaView>
   );
 };
 
