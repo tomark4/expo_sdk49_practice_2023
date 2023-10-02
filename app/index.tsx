59f355cf5666ecc3b4927f3a630b9e66652c5020
Jose Quintero <tomark4@gmail.com>
2023-10-02 14:41:09 -0400

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 22 }}>Hello!</Text>
+    </View>
+  );
+};
+
+export default Home;
